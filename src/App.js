268e713486e425e282c5344e83f5eb26177b1ec6
Lucca Valença <04lucca@gmail.com>
2018-03-10 19:06:38 -0300

    Implementação do Redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import Routes from './Routes';
+import reducers from './reducers';
 
 export default class App extends Component {
   render(){
-    return <Routes />;
+    return (
+      <Provider store={createStore(reducers)}>
+        <Routes />
+      </Provider>
+    );
   }
 }
